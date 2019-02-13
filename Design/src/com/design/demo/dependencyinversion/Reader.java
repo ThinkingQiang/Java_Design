a2ab706e4db64d9a9b028265f2e4e6e736734d6f
sourlemon <work_Xq@163.com>
2019-02-13 18:26:09 +0800

    去除多余内容

--- Design/src/com/design/demo/dependencyinversion/Reader.java
+++
@@ -1,9 +0,0 @@
-package com.design.demo.dependencyinversion;
-
-/**
- * 读取类
- * @author Toni
- */
-public interface Reader {
-	public void getNum(Object o);
-}
--- Design/src/com/design/demo/openandclose/WorkDay.java
+++
@@ -1,9 +0,0 @@
-package com.design.demo.openandclose;
-
-/**
- * 工作日
- * @author Toni
- */
-public interface WorkDay {
-	public void doWork();  //工作方法
-}
